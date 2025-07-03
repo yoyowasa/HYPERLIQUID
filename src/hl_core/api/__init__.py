1fd15dc59515701de0fad463bbeb8af70c3bf3be
Yoshi <yoshiyoshi@example.com>
2025-07-03 10:32:27 +0900

    fix(ws): use set for _subs and guard subscribe

--- src/hl_core/api/__init__.py
+++ src/hl_core/api/__init__.py
@@ -7,7 +7,7 @@ import websockets
 import asyncio
 import anyio
 import contextlib
-from typing import Any, Optional
+from typing import Awaitable, Callable, Any, Optional
 
 logger = logging.getLogger(__name__)
 
